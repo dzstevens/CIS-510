6669b8e39e9a55b3a904d085a320685ba5a229ad
XIAO XIAO <Yvonexiaoxiao@gmail.com>
2013-04-30 16:52:36 -0700

    add sum product variable elimination

--- factor.py
+++ factor.py
@@ -1,48 +1,105 @@
-import sys
-import numpy as np
+from itertools import product
+
+
 class Factor(object):
     
-    def __init__(self, var=[], card=[], val=[], name= 'None'):
+    def __init__(self, var=[], vals=[], card=[]):
         """ a factor has list of variables, each with a cardinality, and for each possible assignment to its variable(s),
-        a position in the val array."""
-        self.var= np.array(var)
-        self.card=np.array(card)
-        self.val=np.array(val)
-        self.name=name
+        a position in the vals array."""
+        self.var= var
+        self.card=card
+        self.vals=vals
+        self.strides = [0]*len(card)
+        prev = None
+        for v in reversed(var):
+            if not prev:
+                self.strides[v] = 1
+            else:
+                self.strides[v] = self.strides[prev]*card[prev]
+            prev = v
 
     def __str__(self):
-        varstring= " ".join ( map(str, self.var) )
-        cardstring=" ".join ( map(str, self.card) )
-        valstring= " ".join( map(str, self.val))
-        return "\n".join( [ 'name: ' + self.name,'var: '+ varstring, 'card: '+ cardstring, 'val: ' + valstring])
+        vars_ = [chr(65+i) for i in self.var]
+        s = '  '.join(vars_)
+        s += '    {:<5}\n'.format(chr(934)+'(' + ', '.join(vars_)+')')
+        line = '–'*len(s) + '\n'
+        s += line
+        for i, assignment in enumerate(product(*[list(range(self.card[n])) for n in self.var])):
+            s += '  '.join([str(x) for x in assignment])
+            s+= '    {:<5.3}\n'.format(self.vals[i])
+        s += line
+        return(s)
+
+    def __mul__(self, other):
+        var = sorted(set(self.var) | set(other.var))
+        psi = dict()
+        j = k = 0
+        v = var[0]
+        assignment = [0]*len(var)
+        for i in range(self.prod([self.card[v] for v in var])):
+            psi[tuple(assignment)] = self.vals[j]*other.vals[k]
+            for l,v in enumerate(var):
+                assignment[l] = (assignment[l] + 1) % self.card[v]
+                if not assignment[l]:
+                    j -= (self.card[v] - 1) * self.strides[v]
+                    k -= (self.card[v] - 1) * other.strides[v]
+                else:
+                    j += self.strides[v] 
+                    k += other.strides[v]
+                    break
+        psi = [psi[k] for k in sorted(psi)]
+        return Factor(var=var,vals=psi, card=self.card)
 
 
-    def setVar(self, var):
-        self.var=np.array(var)
+    def __rmul__(self, other):
+        return self *other
 
-    def getVar(self):
-        return self.var
+    
+    def __imul__(self, other):
+        return self * other
 
-    def getVarCount(self):
-        return len( self.var.tolist() )
+    @staticmethod
+    def prod(l):
+        p = 1
+        for i in l:
+            p *= i
+        return p
 
-    def setVal(self, val):
-        self.val=np.array(val)
+    def ComputeJointDistribution(cliques):
+	return reduce(lambda x,y:x*y,cliquess)
+    
+    def ComputeMarginal(V,F):
+   """
+	V is the variables to be marginalized
+	F is a factor list that contains V
+   """
+	variableList = []
+	for factor in F:
+		var = factor.var
+		variablelist.append(var)
+	union_variables = set().union(*variableList)
+	v = list(union_variables.difference(V))
+	jointE = ComputeJointDistribution(F)
+	jointE_normalizedVal = jointE.vals/np.sum(jointE.vals)
+	jointE.vals = jointE_normalizedVal
+	return FactorMarginalization(jointE,v)
 
-    def getVal(self):
-        return self.val
+    def SumProductEliminaterVar(z,factorList):
+	useFactors = []
+	unusedFactors = []
+	scope = []
+	for fi in factorList:
+		if z in fi.var:
+			useFactors.append(fi)
+			scope = list(set.union(set(scope),fi.var)
+		else:
+			unusedFactors.append(fi)
+	psiFactor = ComputeJointDistribution(useFactors)
+	tauFactor = FactorMarginalization(psiFactor,[z])
+	return unusedFactors+[tauFactor]i
 
-    def setCard(self,card):
-        self.card=np.array(card)
-
-    def getCard(self):
-        return self.card
-
-    def getName(self):
-        return self.name
-
-    def setName(self, name):
-        self.name=name
-
-
-
+    def SumProductVE(self,Z,F):
+	for z in Z:
+		F = SumProductEliminaterVar(z,F)
+	return reduce(lambda x,y:x*y,F)
+ 
